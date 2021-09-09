231dd6c52a3a0dc7b2510bb42fd84112704921f3
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2021-09-09 20:20:44 +0200

    first draft of ReqSelectFrom

--- src/Request.hs
+++ src/Request.hs
@@ -6,6 +6,10 @@ import Data.Char
   ( isLetter
   )
 
+import Data.Maybe
+  ( isJust
+  )
+
 import Parser
   ( Parser
   , many1
@@ -22,14 +26,16 @@ import Json
   )
 
 data Request = ReqExit
-             | ReqSelectFrom ([String],String)
+             | ReqSelectFrom ([String], String)
              | ReqFindWith (String, JsonValue)
              | ReqSetToIn
+  deriving (Show)
 
 pReqSelectFrom :: Parser ([String], String)
 pReqSelectFrom = do
   _ <- matchToken "select"
   c <- many1 il `sepBy` matchToken ","
+  _ <- pSpaces
   _ <- matchToken "from"
   t <- many1 il
   return (c, t)
@@ -66,15 +72,54 @@ isDbValid :: JsonValue -> Bool
 isDbValid (JsonObject ts) = foldr ((&&).isTableValid) True ts
 isDbValid _ = False
 
--- | Request column search
--- trySelectFrom :: [String] -> String -> JsonValue
+-- | Look up key (table name) in a list of tables
+findIndex :: [(String, JsonValue)] -> String -> Maybe Int
+findIndex [] _     = Nothing
+findIndex ((k0,_):ts) k1
+  | k0 == k1  = Just 0
+  | otherwise = Just (1+) <*> findIndex ts k1
 
+-- | Look up table by key
+findTable :: String -> [(String, JsonValue)] -> Maybe JsonValue
+findTable _ [] = Nothing
+findTable k0 ((k1,t):ts)
+  | k0 == k1  = Just t
+  | otherwise = findTable k0 ts
 
--- | Request row search
--- tryFindWith :: 
+extrIndex :: Int -> JsonValue -> JsonValue
+extrIndex i (JsonArray xs) = xs !! i
+extrIndex _ _ = error "invalid operation"
+
+-- | Extract certain column from the table
+extrCol :: [JsonValue] -> Maybe Int -> [JsonValue]
+extrCol rows (Just i) = map (extrIndex i) rows
+extrCol _ _ = error "invalid operation"
+
+-- | Extract predefined multiple columns from the table
+extrMulCols :: [Maybe Int] -> JsonValue -> [[JsonValue]]
+extrMulCols ids (JsonArray rows) = foldr ((:).extrCol rows) [] ids
+extrMulCols _ _ = error "invalid operation" -- will not happen
 
 -- | Try to apply well-formed request and return new database
 tryApplyReq :: Request -> JsonValue -> IO JsonValue
 tryApplyReq ReqExit t = pure t
-tryApplyReq (ReqSelectFrom (cs, t)) jv = do
-  return jv
+
+tryApplyReq (ReqSelectFrom (cs, t)) j@(JsonObject ts) = do
+  let tb = findTable t ts
+  case tb of
+    Nothing -> do
+      putStrLn "invalid request"
+      return j
+    Just tx -> do
+      let indices = map (findIndex ts) cs
+      let valid   = foldr ((&&).isJust) True indices
+      if not valid then do
+        putStrLn "invalid request"
+        return j
+      else do      -- columns to be extracted from the (t)able
+        let _ = map (print.JsonArray) (extrMulCols indices tx)
+        return j
+
+tryApplyReq (ReqFindWith _) t = pure t
+tryApplyReq ReqSetToIn      t = pure t
+tryApplyReq _               t = pure t
