cc5ef28685e2c858cc528cd53bab533549187e84
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2021-09-09 15:45:43 +0200

    json value Eq instance (struct. equality)

--- src/Json.hs
+++ src/Json.hs
@@ -32,6 +32,16 @@ jsitem2str :: (String, JsonValue) -> String -> String
 jsitem2str (k, v) "" = show k <> ":" <> show v
 jsitem2str (k, v) s = show k <> ":" <> show v <> ", " <> s
 
+instance Eq JsonValue where
+
+  (==) JsonNull        JsonNull        = True
+  (==) (JsonBool   b1) (JsonBool b2)   = b1 == b2
+  (==) (JsonString s1) (JsonString s2) = s1 == s2
+  (==) (JsonNumber n1) (JsonNumber n2) = n1 == n2
+  (==) (JsonArray  a1) (JsonArray  a2) = a1 == a2
+  (==) (JsonObject o1) (JsonObject o2) = and $ zipWith (\ (s1,j1) (s2,j2) -> s1 == s2 && j1 == j2) o1 o2
+  (==) _               _               = False
+
 instance Show JsonValue where
 
   show JsonNull        = "null"
